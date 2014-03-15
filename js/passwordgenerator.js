c61e514b2469ff97c841c439e85c55ce76f3e123
Jan Horava <czechinthemail@gmail.com>
2014-03-15 13:11:23 -0700

    removed get() function, improved formatting

--- js/passwordgenerator.js
+++ js/passwordgenerator.js
@@ -5,10 +5,10 @@ function generate(){
         temp = '';
 
         // create new password
-        j = get(7).value - 1;
+        j = document.getElementById(7).value - 1;
         do{
             // add character to current password
-            i = get(0).value - 1;
+            i = document.getElementById(0).value - 1;
             do{
                 // select random characters from possible character list
                 temp += charlist.substr(Math.floor(Math.random() * charlist.length - 1), 1);
@@ -17,29 +17,25 @@ function generate(){
             temp += '<br>';
         }while(j--);
 
-        get('password').innerHTML = temp;
+        document.getElementById('password').innerHTML = temp;
         temp = '';
 
     }else{
-        get('password').innerHTML = 'You must select at least one option.';
+        document.getElementById('password').innerHTML = 'You must select at least one option.';
     }
-}
-
-function get(i){
-    return document.getElementById(i);
 }
 
 function reset(){
     if(confirm('Reset settings?')){
-        get(0).value = 15;
-        get(1).checked = 1;
-        get(2).checked = 1;
-        get(3).checked = 1;
-        get(4).checked = 2;
-        get(5).checked = 3;
-        get(6).checked = 1;
-        get(7).value = 1;
-        get('password').innerHTML = '';
+        document.getElementById(0).value = 15;
+        document.getElementById(1).checked = 1;
+        document.getElementById(2).checked = 1;
+        document.getElementById(3).checked = 1;
+        document.getElementById(4).checked = 2;
+        document.getElementById(5).checked = 3;
+        document.getElementById(6).checked = 1;
+        document.getElementById(7).value = 1;
+        document.getElementById('password').innerHTML = '';
 
         save();
     }
@@ -47,25 +43,29 @@ function reset(){
 
 function save(){
     // validate settings
-    if(isNaN(get(0).value) || get(0).value < 1 || get(0).value == 15){
-        get(0).value = 15;
-        ls.removeItem('password-generator-0');
+    if(isNaN(document.getElementById(0).value)
+      || document.getElementById(0).value < 1
+      || document.getElementById(0).value == 15){
+        document.getElementById(0).value = 15;
+        window.localStorage.removeItem('password-generator-0');
 
     }else{
-        ls.setItem(
+        window.localStorage.setItem(
             'password-generator-0',
-            get(0).value
+            document.getElementById(0).value
         );
     }
 
-    if(isNaN(get(7).value) || get(7).value < 1 || get(7).value == 1){
-        get(7).value = 1;
-        ls.removeItem('password-generator-7');
+    if(isNaN(document.getElementById(7).value)
+      || document.getElementById(7).value < 1
+      || document.getElementById(7).value == 1){
+        document.getElementById(7).value = 1;
+        window.localStorage.removeItem('password-generator-7');
 
     }else{
-        ls.setItem(
+        window.localStorage.setItem(
             'password-generator-7',
-            get(7).value
+            document.getElementById(7).value
         );
     }
 
@@ -73,7 +73,7 @@ function save(){
     i = 5;
     charlist = '';
     do{
-        if(get(i + 1).checked){
+        if(document.getElementById(i + 1).checked){
             if(i === 0){
                 // lowercase letters
                 charlist += 'abcdefghijklmnopqrstuvwxyz';
@@ -99,10 +99,10 @@ function save(){
                 charlist += 'ÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ';
             }
 
-            ls.removeItem('password-generator-' + (i + 1));
+            window.localStorage.removeItem('password-generator-' + (i + 1));
 
         }else{
-            ls.setItem(
+            window.localStorage.setItem(
                 'password-generator-' + (i + 1),
                 1
             );
@@ -113,17 +113,20 @@ function save(){
 var charlist = '';
 var i = 0;
 var j = 5;
-var ls = window.localStorage;
 var temp = '';
 
-get(0).value = ls.getItem('password-generator-0') === null ? 15 : ls.getItem('password-generator-0');
-get(1).checked = ls.getItem('password-generator-1') == null;
-get(2).checked = ls.getItem('password-generator-2') == null;
-get(3).checked = ls.getItem('password-generator-3') == null;
-get(4).checked = ls.getItem('password-generator-4') == null;
-get(5).checked = ls.getItem('password-generator-5') == null;
-get(6).checked = ls.getItem('password-generator-6') == null;
-get(7).value = ls.getItem('password-generator-7') === null ? 1 : ls.getItem('password-generator-7');
+document.getElementById(0).value = window.localStorage.getItem('password-generator-0') === null
+  ? 15
+  : window.localStorage.getItem('password-generator-0');
+document.getElementById(1).checked = window.localStorage.getItem('password-generator-1') == null;
+document.getElementById(2).checked = window.localStorage.getItem('password-generator-2') == null;
+document.getElementById(3).checked = window.localStorage.getItem('password-generator-3') == null;
+document.getElementById(4).checked = window.localStorage.getItem('password-generator-4') == null;
+document.getElementById(5).checked = window.localStorage.getItem('password-generator-5') == null;
+document.getElementById(6).checked = window.localStorage.getItem('password-generator-6') == null;
+document.getElementById(7).value = window.localStorage.getItem('password-generator-7') === null
+  ? 1
+  : window.localStorage.getItem('password-generator-7');
 
 window.onkeydown = function(e){
     i = window.event ? event : e;
