4ef6b713ef23b92cfaa6afc73b6ec319d34e178a
honzi <czechinthemail@gmail.com>
2016-07-17 17:51:17 -0700

    added random_string() usage

--- js/passwordgenerator.js
+++ js/passwordgenerator.js
@@ -26,26 +26,13 @@ function generate(){
     var passwords = '';
 
     // Create new password.
-    var character_looptotal = settings_settings['length'] - 1;
-    var password_loopcounter = settings_settings['number-of-passwords'] - 1;
+    var loopcounter = settings_settings['number-of-passwords'] - 1;
     do{
-        // Add character to current password.
-        var character_loopcounter = character_looptotal;
-        do{
-            // Select random characters from possible character list...
-            //   ...handle the HTML symbols...
-            //   ...and add it to the passwords string.
-            passwords += charlist.substr(
-              random_integer(charlist.length - 1),
-              1
-            ).replace(/&/g,'&amp;')
-             .replace(/>/g,'&gt;')
-             .replace(/</g,'&lt;')
-             .replace(/"/g,'&quot;');
-        }while(character_loopcounter--);
-
-        passwords += '<br>';
-    }while(password_loopcounter--);
+        passwords += random_string(
+          settings_settings['length'] - 1,
+          charlist
+        ) + '<br>';
+    }while(loopcounter--);
 
     document.getElementById('passwords').innerHTML = passwords;
 }
