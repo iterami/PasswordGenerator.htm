eabc90d7474b471771385c9fca40c80994558a6a
Jan Hořava <czechinthemail@gmail.com>
2018-12-04 14:37:18 -0800

    increased default length to 64

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function repo_init(){
       'menu-block-events': false,
       'storage': {
         'characters': '0123456789abcdefghijklmnopqrstuvwxyzáčďéěíňóřšťúůýžABCDEFGHIJKLMNOPQRSTUVWXYZÁČĎÉĚÍŇÓŘŠŤÚŮÝŽ~!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?',
-        'length': 15,
+        'length': 64,
         'repeat': 1,
       },
       'storage-menu': '<textarea id=characters></textarea><table><tr><td><input id=length><td>Length<tr><td><input id=repeat><td>Repeat</table>',
