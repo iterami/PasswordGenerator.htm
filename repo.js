321c94bc2d937e0c1e4679d7d9c559bdd52fff2a
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 14:10:25 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,31 @@
 'use strict';
 
+function generate(){
+    core_storage_save({
+      'keys': [
+        'characters',
+        'length',
+        'repeat',
+      ],
+    });
+
+    let loopcounter = core_storage_data['repeat'] - 1;
+    let passwords = '';
+    do{
+        passwords += core_html_format({
+          'string': core_random_string({
+            'characters': core_storage_data['characters'],
+            'length': core_storage_data['length'] - 1,
+          }),
+        });
+
+        if(loopcounter > 0){
+            passwords += '\n\n';
+        }
+    }while(loopcounter--);
+    document.getElementById('passwords').value = passwords;
+}
+
 function repo_init(){
     core_repo_init({
       'events': {
