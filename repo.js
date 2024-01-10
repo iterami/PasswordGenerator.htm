4200b975c8124369a777f6d22e37138635a5958c
Jan Hořava <czechinthemail@gmail.com>
2024-01-10 04:20:36 -0800

    updated core_html_format() usage

--- repo.js
+++ repo.js
@@ -12,12 +12,10 @@ function generate(){
     let loopcounter = core_storage_data['repeat'] - 1;
     let passwords = '';
     do{
-        passwords += core_html_format({
-          'string': core_random_string({
-            'characters': core_storage_data['characters'],
-            'length': core_storage_data['length'] - 1,
-          }),
-        });
+        passwords += core_html_format(core_random_string({
+          'characters': core_storage_data['characters'],
+          'length': core_storage_data['length'] - 1,
+        }));
 
         if(loopcounter > 0){
             passwords += '\n\n';
